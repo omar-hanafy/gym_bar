48f61953a4203a47748d5089cb537aa1df30c7b4
omarkhaled <devomar615@gmail.com>
2019-12-11 16:02:15 +0200

    reformat project

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:gym_bar/router.dart';
 import 'package:provider/provider.dart';
-import 'core/models/product_crud.dart';
-import 'locator.dart';
 
 void main() {
   runApp(MyApp());
-  setupLocator();
+  // setupLocator();
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(builder: (_) => locator<ProductCrud>()),
+        // ChangeNotifierProvider(builder: (_) => locator<ProductCrud>()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         title: 'Gym Caffe',
         theme: ThemeData(),
-        onGenerateRoute: Router.generateRoute,
+        // onGenerateRoute: Router.generateRoute,
       ),
     );
   }
-}
+}x
